3a03e0891a616f3e6ef4e5a8a9bc687157421096
Carl Sutherland <carl@humblesoftware.com>
2016-07-09 17:56:23 -0700

    Initial server module.

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Module:
+var browserSync = require('browser-sync');
+
+module.exports = {
+  browserSync: browserSync,
+  serverTask: serverTask
+}
+
+function serverTask (options) {
+
+  options = config(options);
+
+  // Task:
+  return function () {
+    browserSync.init({
+      port: options.port,
+      server: options.root
+    });
+  }
+}
+
+function config (options) {
+
+  // Defaults:
+  options      = options || {};
+  options.port = options.port || 8080;
+  options.root = options.root || './build';
+
+  return options;
+}
